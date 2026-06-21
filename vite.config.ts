dd573cad3c12988097ada621b77c0f659f71d9a7
suppandochka <suppandochka@gmail.com>
2026-06-21 23:12:40 +0500

    I-hope-its-gonna-work-plz

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'dentist-lending-site.github.io',
+  base: '/dentist-lending-site.github.io/',
 
   plugins: [react()],
   server: { host: '127.0.0.1', port: 5173 },
